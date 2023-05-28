b73c142b399a73cdbf6ffbf11868b836fcc95f32
quinton98x <126735634+quinton98x@users.noreply.github.com>
2023-05-27 14:52:43 -1000

    Update MUTEnsRenewalController.sol

--- contract/MUTEnsRenewalController.sol
+++ contract/MUTEnsRenewalController.sol
@@ -1,1 +1,21 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MUTEnsRegistry.sol";
+import "./MUTEnsFees.sol";
+
+contract MUTEnsRenewalController is Ownable {
+    MUTEnsRegistry public registry;
+    MUTEnsFees public fees;
+
+    constructor(MUTEnsRegistry _registry, MUTEnsFees _fees) {
+        registry = _registry;
+        fees = _fees;
+    }
+
+    function renew(string memory domain) public payable {
+        require(msg.value == fees.getRegistrationFee(), "Renewal fee does not match the required fee");
+        registry.registerDomain(domain, "", address(this));
+    }
+}
 
