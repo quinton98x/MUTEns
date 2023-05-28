02d51c0a504fb9d98f0eb4ca16cfab0ef4f7552f
quinton98x <126735634+quinton98x@users.noreply.github.com>
2023-05-27 15:02:14 -1000

    Update MUTEnsRegistry.sol

--- contract/MUTEnsRegistry.sol
+++ contract/MUTEnsRegistry.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -22,30 +21,21 @@ contract MUTEnsRegistry is ERC721, Ownable, Pausable {
     constructor() ERC721("MUTE Domain Name Service", "MUTEns") {}
 
     // Permits modifications only by the owner of the specified node.
-    modifier authorised(string memory domain) {
-        address owner = domains[domain].owner;
-        require(owner == _msgSender() || isApprovedForAll(owner, _msgSender()), "Caller is not the owner");
+    modifier onlyDomainOwner(string memory domain) {
+        require(domains[domain].owner == _msgSender(), "Caller is not the owner");
         _;
     }
 
-    function registerDomain(string memory domain, string memory muteAddress, address resolver) external authorised(domain) {
+    function registerDomain(string memory domain, string memory muteAddress, address resolver) external onlyDomainOwner(domain) {
         // Registration logic here...
+        domains[domain] = Domain(_msgSender(), domains[domain].tokenId, muteAddress, resolver);
+        emit DomainRegistered(domain, _msgSender(), domains[domain].tokenId, muteAddress, resolver);
     }
 
-    function transferDomain(string memory domain, address newOwner) external authorised(domain) {
+    function transferDomain(string memory domain, address newOwner) external onlyDomainOwner(domain) {
         // Transfer logic here...
-    }
-
-    function setResolver(string memory domain, address resolver) public authorised(domain) {
-        domains[domain].resolver = resolver;
-        emit DomainRegistered(domain, domains[domain].owner, domains[domain].tokenId, domains[domain].muteAddress, resolver);
-    }
-
-    function owner(string memory domain) public view returns (address) {
-        return domains[domain].owner;
-    }
-
-    function resolver(string memory domain) public view returns (address) {
-        return domains[domain].resolver;
+        domains[domain].owner = newOwner;
+        emit DomainTransferred(domain, newOwner);
     }
 }
+
