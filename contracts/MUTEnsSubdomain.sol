4ec8d45cb901c01bc335cdd74952d463b5661fd1
quinton98x <126735634+quinton98x@users.noreply.github.com>
2023-05-27 16:27:57 -1000

    Update MUTEnsSubdomain.sol

--- contracts/MUTEnsSubdomain.sol
+++ contracts/MUTEnsSubdomain.sol
@@ -1,1 +1,18 @@
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MUTEnsRegistry.sol";
+
+contract MUTEnsSubdomains is Ownable {
+    MUTEnsRegistry public registry;
+
+    constructor(MUTEnsRegistry _registry) {
+        registry = _registry;
+    }
+
+    function registerSubdomain(string memory domain, string memory subdomain, address owner, address resolver) public onlyOwner {
+        string memory fullDomain = string(abi.encodePacked(subdomain, ".", domain));
+        registry.registerDomain(fullDomain, "", resolver);
+        registry.transferDomain(fullDomain, owner);
+    }
+}
