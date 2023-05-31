1b03a184ba51fabbf89c5d9e7eb008ef4c8e6750
quinton98x <126735634+quinton98x@users.noreply.github.com>
2023-05-30 22:12:50 -1000

    Update MUTEnsSubdomainRegistrar.sol

--- contracts/MUTEnsSubdomainRegistrar.sol
+++ contracts/MUTEnsSubdomainRegistrar.sol
@@ -10,9 +10,27 @@ contract MUTEnsSubdomainRegistrar is Ownable {
         registry = _registry;
     }
 
-    function registerSubdomain(string memory subdomain, string memory domain) public {
-        require(registry.owner(domain) == msg.sender, "Caller is not the owner of the domain");
-        registry.registerDomain(subdomain, "", address(this));
+    function registerSubdomain(string memory subdomain, string memory domain, address muteAddress) public {
+        require(muteAddress != address(0), "MuteAddress is required");
+        (address domainOwner,,,,) = registry.domains(domain);
+        require(domainOwner == msg.sender, "Caller is not the owner of the domain");
+
+        string memory muteAddressStr = addressToString(muteAddress);
+        string memory subdomainName = string(abi.encodePacked(subdomain, ".", muteAddressStr));
+        registry.registerDomain(subdomainName, muteAddressStr, address(this));
+    }
+
+    function addressToString(address _address) internal pure returns (string memory) {
+        bytes32 value = bytes32(uint256(uint160(_address)));
+        bytes memory alphabet = "0123456789abcdef";
+
+        bytes memory str = new bytes(42);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint256 i = 0; i < 20; i++) {
+            str[2 + i * 2] = alphabet[uint8(value[i + 12] >> 4)];
+            str[3 + i * 2] = alphabet[uint8(value[i + 12] & 0x0f)];
+        }
+        return string(str);
     }
 }
-
