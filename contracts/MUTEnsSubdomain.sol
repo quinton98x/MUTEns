e67fee19e761b6cf3057cf08a899005768a4222d
quinton98x <126735634+quinton98x@users.noreply.github.com>
2023-05-31 00:34:21 -1000

    Update MUTEnsSubdomain.sol

--- contracts/MUTEnsSubdomain.sol
+++ contracts/MUTEnsSubdomain.sol
@@ -1,18 +1,25 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "./MUTEnsRegistry.sol";
 
-contract MUTEnsSubdomains is Ownable {
+contract MUTEnsSubdomainRegistrar is Ownable, Pausable {
     MUTEnsRegistry public registry;
+
+    event SubdomainCreated(string indexed domain, string subdomain, address indexed owner);
 
     constructor(MUTEnsRegistry _registry) {
         registry = _registry;
     }
 
-    function registerSubdomain(string memory domain, string memory subdomain, address owner, address resolver) public onlyOwner {
-        string memory fullDomain = string(abi.encodePacked(subdomain, ".", domain));
-        registry.registerDomain(fullDomain, "", resolver);
-        registry.transferDomain(fullDomain, owner);
+    function createSubdomain(string memory domain, string memory subdomain, address owner) public onlyOwner {
+        // Ensure the msg.sender is the owner of the domain
+        require(registry.ownerOf(domain) == msg.sender, "Caller is not the owner of the domain");
+
+        // Create the subdomain
+        registry.registerSubdomain(domain, subdomain, owner);
+
+        emit SubdomainCreated(domain, subdomain, owner);
     }
 }
